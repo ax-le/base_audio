e072c7995347e319d08e598c6116ba2c9355988d
Axel (portable) <axel.marmoret@imt-atlantique.fr>
2024-12-20 12:24:31 +0100

    Pip update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="base_audio",
-    version="0.1.0",
+    version="0.1.1",
     author="Marmoret Axel",
     author_email="axel.marmoret@imt-atlantique.fr",
     description="Package for handling audio files.",
